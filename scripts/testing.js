bcde680c5a9de81e4e1de35f7cd22b4d41a8c990
Milan Sreckovic <msreckovic@yahoo.com>
2019-03-06 22:09:35 -0500

    Update testing.js

--- scripts/testing.js
+++ scripts/testing.js
@@ -4,7 +4,7 @@ if(ss){
     "<h2>Click the Accept button at bottom left, and follow the payment instructions.</h2>";
 }
 
-var sc=window.location.href.includes("Completed");
+var sc=window.location.href.includes("Complete");
 if(sc){
   document.getElementById("arcextra").innerHTML =
     "<h2><a href=\"https://argonaut-rowing-club.myshopify.com/cart/16140123537510:1?note=" +
