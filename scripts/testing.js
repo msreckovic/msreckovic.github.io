b7dcb92b0eca699018c8cfd2bb1ea50e2d501c0f
Milan Sreckovic <msreckovic@yahoo.com>
2019-03-06 21:13:12 -0500

    Create testing.js

---
+++ scripts/testing.js
@@ -0,0 +1,2 @@
+var ss=window.location.href.includes("Agree");if(true){document.getElementById("arcagree3").innerHTML = "<h2>Click the Accept button at bottom left, and follow the payment instructions.</h2>";}
+var sc=window.location.href.includes("Complete");if(true){document.getElementById("arccomplete3").innerHTML = "<h2><a href=\"https://argonaut-rowing-club.myshopify.com/cart/16140123537510:1?note=" + document.getElementById("HeadLoginName").innerHTML + "-for-2019-membership\">Click here to open the Argo store and complete the payment</a></h2>";}
