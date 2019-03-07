1269791a5162d761b783f6782f518781cac8718f
Milan Sreckovic <msreckovic@yahoo.com>
2019-03-06 23:01:10 -0500

    Update testing.js

--- scripts/testing.js
+++ scripts/testing.js
@@ -10,4 +10,21 @@ if(sc){
     "<h2><a href=\"https://argonaut-rowing-club.myshopify.com/cart/16140123537510:1?note=" +
     document.getElementById("HeadLoginName").innerHTML +
     "-for-2019-membership\">Click here to open the Argo store and complete the payment</a></h2>";
+
+  var all = document.getElementsByTagName("a");
+  for (var i=0; i<all.length; i++) {
+    var el = all[i];
+    if (el.innerHTML.includes("Pay Online Now")) {
+      if (el.hasAttributes()) {
+        var attrs = el.attributes;
+        for (var j=0; j<attrs.length; j++) {
+          if (attrs[j].name.includes("href")) {
+            attrs[j].value = "https://www.google.com/";
+          }
+        }
+      }
+    }
+  }
+
+
 }
