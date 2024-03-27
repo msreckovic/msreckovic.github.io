aeb40725b3ed489b4ff42e7d17fe238c07c304be
Milan Sreckovic <msreckovic@yahoo.com>
2024-03-26 21:41:09 -0400

    Simplified messages, Matt for email

--- scripts/arc/regattainfo.js
+++ scripts/arc/regattainfo.js
@@ -69,7 +69,7 @@ function CallbackListRegattasHow(jsonIn, active)
       ln = "?regatta=" + sn;
       detail += "<a target=\"_blank\" href=\"" + ln + "\">click for details</a>";
     } else if (active) {
-      detail += "<a href=\"mailto:captain@argonautrowingclub.com\">click to inquire</a>";
+      detail += "<a href=\"mailto:matt@argonautrowingclub.com\">click to inquire</a>";
     }
     if (!detail) {
       detail = "&nbsp;";
@@ -112,7 +112,7 @@ function InfoSingleRegatta(entry)
   total += "<ul>";
   total += GV(entry, "details");
   if (when && location) {
-    total += "<li>It is held " + when + " " + location + "</li>";
+    total += "<li>The regatta is held on " + when + " at " + location + "</li>";
   }
   if (entrydue) {
     total += "<li>Entry deadline (see below for details) is " + entrydue + ".</li>";
@@ -123,23 +123,22 @@ function InfoSingleRegatta(entry)
   if (unloading) {
     total += "<li>Boats are unloaded on " + unloading + ". Each crew must have enough members present to safely unload the trailer.</li>";
   }
-  total += "<li>Outstanding entry and trailer fees should be paid before the boats are loaded.</li>";
-  total += "<li>See the bottom of this page for current entries and outstanding fees.</li>";
+  total += "<li>See the bottom of this page for the current entries and outstanding fees.</li>";
+  total += "<li>For more details and event list and timing, go to <a target=\"_blank\" href=\"http://regattacentral.com\">regattacentral.com</a></li>";
   total += "</ul>";
-  total += "For more details and event list and timing, go to <a target=\"_blank\" href=\"http://regattacentral.com\">regattacentral.com</a>";
   return total;
 }
 
 function InfoSingleEntries(entry)
 {
   var total = "";
-  total += "If unsure what to do, ask <a href=\"mailto:captain@argonautrowingclub.com\">captain@argonautrowingclub.com</a>.";
-  total += "<ol>";
+  total += "<ul>";
   total += GV(entry, "entries");
-  total += "<li>Everybody in the crew needs to be in <a style=\"color:red;\" target=\"_blank\" href=\"http://www.argonautrowingclub.com/member-roster/\">the club roster list</a> or the software won't let us enter them.  This means at least three or four days earlier for the RCA registration and fees payment.</li>";
-  total += '<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSccnTZU3WQ9kTt-9VQwg75VCw_nGWsw9U-wVShsU_thSHzHDQ/viewform">Click here for the form for submitting entries.</a></li>';
-  total += "<li>Send an e-mail to <a href=\"mailto:captain@argonautrowingclub.com\">captain@argonautrowingclub.com</a>, with any questions or problems.</li>";
-  total += "</ol>";
+  total += "<li>Everybody in the crew needs to be in an active club member, registered with RCA.</li>";
+  total += "<li>Talk to your program coach or coordinator about entry submissions.</li>";
+  total += "<li>Once the entries are submitted, you are responsible for the payment, whether you race or not.</li>";
+  total += "<li>Send an e-mail to <a href=\"mailto:matt@argonautrowingclub.com\">Matt</a>, with any questions or problems.</li>";
+  total += "</ul>";
   return total;
 }
 
@@ -150,10 +149,7 @@ function InfoSinglePayment(entry)
   total += "<ul>";
   total += GV(entry, "payment");
   total += "<li>Find your name in the list below, see what your total fees are.</li>";
-  total += "<li>Payments are due before the trailer is loaded.</li>";
-  total += "<li>Payments made after the trailer loading are subject to a $5 late fee.</li>";
-  total += "<li>Payments made after the athlete raced their first race at a regatta are subject to a $10 late fee.</li>";
-  total += "<li>Payments made after the regatta date are subject to a $15 late fee.</li>";
+  total += "<li>Please monitor the email you registered with, that is where you will get the payment reminders.</li>";
   total += "</ul>";
   return total;
 }
@@ -165,7 +161,7 @@ function InfoSingleFAQ(entry)
   total += "<ul>";
   total += "<li>You have to be a registered ARC member, as well as registered with RCA. Without that in place, we cannot add you to the crew roster in the regatta registration system.</li>";
   total += "<li>Getting all the things in place will take a few days, so please submit the entries as early as possible.</li>";
-  total += "<li>If you are rowing in a composite crew, with non-ARC members, start the process a bit earlier. Note that the trailer fees are doubled for non-ARC members.</li>";
+  total += "<li>If you are rowing in a composite crew, with non-ARC members, start the process earlier. Note that the trailer fees are doubled for non-ARC members.</li>";
   total += "<li>Note that you may not get the first choice of a boat for the race.</li>";
   total += "</ul>";
   return total;
@@ -189,10 +185,6 @@ function CallbackForRegatta(entry)
   el = document.getElementById("payment");
   if (el) {
     el.innerHTML = InfoSinglePayment(entry);
-  }
-  el = document.getElementById("faq");
-  if (el) {
-    el.innerHTML = InfoSingleFAQ(entry);
   }
 }
 
@@ -231,22 +223,21 @@ function StandardRegattaConfiguration(shortname, yearsheet, regattasheet)
 {
   RegattaSpecificName = shortname;
   var total = "";
-  total += "<h2>Regatta Details & Deadlines</h2><div id=\"regatta\"></div>\n";
-  total += "<h2>Entries</h2><div id=\"entries\"></div>\n";
-  total += "<h2>Payment</h2><div id=\"payment\"></div>\n";
-  total += "<h2>FAQ</h2><div id=\"faq\"></div>\n";
+  total += "<h3>Regatta Details & Deadlines</h3><div id=\"regatta\"></div>\n";
+  total += "<h3>Entries</h3><div id=\"entries\"></div>\n";
+  total += "<h3>Payment</h3><div id=\"payment\"></div>\n";
   total += "<hr>\n";
-  total += "<h2>Current Registration Information</h2>\n";
+  total += "<h3>Current Registration Information</h3>\n";
   total += "<h4 id=\"title\">Regatta</h4>\n";
-  total += "<h3>Summary</h3><div id=\"summary\"></div>\n";
+  total += "<h4>Summary</h4><div id=\"summary\"></div>\n";
   total += "<hr />\n";
-  total += "<h3>Athletes</h3><div id=\"athletes\"></div>\n";
+  total += "<h4>Athletes</h4><div id=\"athletes\"></div>\n";
   total += "<hr />\n";
-  total += "<h3>Event List</h3><div id=\"details\"></div>\n";
+  total += "<h4>Event List</h4><div id=\"details\"></div>\n";
   total += "<hr />\n";
-  total += "<h3>Boats</h3><div id=\"boats\"></div>\n";
+  total += "<h4>Boats</h4><div id=\"boats\"></div>\n";
   total += "<hr />\n";
-  total += "<h3>Trailer Map</h3><div id=\"trailermap\"></div>\n";
+  total += "<h4>Trailer Map</h4><div id=\"trailermap\"></div>\n";
 
   console.log("Calling with yearsheet " + yearsheet);
   console.log("Calling with regattasheet " + regattasheet);
