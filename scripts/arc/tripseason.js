82d1b18fbbd8531c1b41c92aff613cd78a53ef9b
Milan Sreckovic <msreckovic@yahoo.com>
2024-10-18 21:04:47 -0400

    Update tripseason.js for messages

--- scripts/arc/tripseason.js
+++ scripts/arc/tripseason.js
@@ -13,29 +13,47 @@ function GetQueryParameters(what) {
   return undefined;
 }
 
-function TeslaTripsYears(where)
+function TeslaTripsYears()
 {
   var total = "";
   total += "<h3>";
-  total += "<a href=\"?year=2024\">2024</a>,";
-  total += "<a href=\"?year=2023\">2023</a>,";
-  total += "<a href=\"?year=2022\">2022</a>,";
-  total += "<a href=\"?year=2021\">2021</a>,";
-  total += "<a href=\"?year=2020\">2020</a>,";
-  total += "<a href=\"?year=2019\">2019</a>,";
-  total += "<a href=\"?year=2018\">2018</a>";
-  total += "<a href=\"?year=plans\">Plans</a>";
+  total += "<a href=\"?year=2024\">2024</a>, ";
+  total += "<a href=\"?year=2023\">2023</a>, ";
+  total += "<a href=\"?year=2022\">2022</a>, ";
+  total += "<a href=\"?year=2021\">2021</a>,<br>";
+  total += "<a href=\"?year=2020\">2020</a>, ";
+  total += "<a href=\"?year=2019\">2019</a>, ";
+  total += "<a href=\"?year=2018\">2018</a>,<br>";
+  total += "<a href=\"?year=plans\">Plans</a>, ";
   total += "<a href=\"?year=live\">Live</a>";
   total += "</h3>";
-  document.getElementById(where).innerHTML = total;
+  return total;
+}
+
+function Message()
+{
+ var googleSheet = GoogleSheetSquare;
+
+  var script_tag = document.getElementById('tripseason')
+  if (script_tag) {
+    var argsheet = script_tag.getAttribute("data-sheet");
+    if (argsheet) {
+      console.log("Setting tripseason.js.GoogleSheetSquare from data-sheet to " + argsheet)
+      googleSheet = argsheet;
+    }
+    var ksheet = script_tag.getAttribute("k");
+    GetAllData(TeslaMessage, googleSheet, "messages", ksheet);
+  }
 }
 
 function StandardTripYear()
 {
   if (TripYear == "All Years") {
-    TeslaTripsYears("trip");
+    Message();
     return;
   }
+
+  document.getElementById("trip-title").innerHTML = TripYear;
 
   var googleSheet = GoogleSheetSquare;
 
@@ -51,11 +69,23 @@ function StandardTripYear()
   }
 }
 
+function TeslaMessage(values)
+{
+  var total = "";
+  if (values.length > 1) {
+    var i;
+    for (i = values.length - 1; i > 0; i--) {
+      total += "<h4><b>" + values[i][0] + "</b>: " + values[i][1] + "</h4>";
+    }
+  }
+
+  total += TeslaTripsYears();
+  document.getElementById("trip").innerHTML = total;
+}
+
 var TripYear = GetQueryParameters("year");
 if ("" == TripYear || void 0 == TripYear) {
   TripYear = "All Years";
 }
 console.log("Trip year " + TripYear);
-
-document.getElementById("trip-title").innerHTML = TripYear;
 StandardTripYear(TripYear);
